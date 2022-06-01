ea9b879dcd7e53abb1ce5f55c09e4dca13fb19cb
Libes6 <fya-07@list.ru>
2022-06-01 09:36:44 +0300

    add scss

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
+  let max = ["Максим Кулаков", "React js разработчик","Люблю пиццу"]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
     </div>
   );
 }
