0324f20718a5b19b2715a5242c51d9ff649d8d4a
Max <88722440+Libes6@users.noreply.github.com>
2022-10-11 22:07:39 +0700

    refresh

--- src/App.jsx
+++ src/App.jsx
@@ -18,36 +18,20 @@ function App() {
   const [searchValue, setSearchValue] = React.useState("");
 
   return (
-    <div>
-    <div>
-      <button
-        aria-label="Increment value"
-        onClick={() => dispatch(increment())}
-      >
-        Increment
-      </button>
-      <span>{count}</span>
-      <button
-        aria-label="Decrement value"
-        onClick={() => dispatch(decrement())}
-      >
-        Decrement
-      </button>
-    </div>
-  </div>
-    // <AppContex.Provider value={{ searchValue, setSearchValue }}>
-    //   <div className="wrapper">
-    //     <Header />
-    //     <div className="content">
-    //       <Routes>
-    //         <Route path="/" element={<Home />} />
-    //         <Route path="/cart" element={<Cart />} />
+  
+    <AppContex.Provider value={{ searchValue, setSearchValue }}>
+      <div className="wrapper">
+        <Header />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
 
-    //         <Route path="*" element={<NotFound />} />
-    //       </Routes>
-    //     </div>
-    //   </div>
-    // </AppContex.Provider>
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+      </div>
+    </AppContex.Provider>
   );
 }
 
